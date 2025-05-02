e2ec302a9788c12830da5bb7e6f067a56ca421f8
S-Garner <143762033+S-Garner@users.noreply.github.com>
2025-05-02 02:21:02 -0500

    test

---
+++ gameLogic/Chamber.js
@@ -0,0 +1,18 @@
+// const { Bullet } = require('./Bullet');
+const { Loader } = require('./Loader');
+
+class Chamber {
+	constructor(count) {
+		const loader = new Loader(count);
+		this.bullets = loader.getBullets();
+		console.log(this.bullets.map(b => b.getType()));
+	}
+
+	toString() {
+		// Example: return a comma-separated list of bullet types
+		const bulletTypes = this.bullets.map(b => b.getType());
+		return `Chamber loaded with: ${bulletTypes.join(', ')}`;
+	}
+}
+
+module.exports = Chamber;
---
+++ gameLogic/Loader.js
@@ -0,0 +1,54 @@
+const { Bullet } = require('./Bullet');
+
+class Loader {
+	constructor(bCount) {
+		this.bullets = [];
+		const ratios = [
+			[1, 1],
+			[2, 1],
+			[3, 1],
+			[4, 1],
+			[2, 2],
+			[2, 3],
+			[2, 4],
+			[3, 3],
+			[3, 4],
+			[3, 5],
+			[4, 4],
+		];
+		this.setBullets(ratios, bCount);
+	}
+
+	getBullets() {
+		return this.bullets;
+	}
+
+	setBullets(ratios, count) {
+		const possibleRatios = [];
+		for (let i = 0; i < ratios.length; i++) {
+			if (ratios[i][0] + ratios[i][1] === count) {
+				possibleRatios.push(ratios[i]);
+			}
+		}
+		if (possibleRatios.length === 0) {
+			throw new Error(`No valid ratios found for bullet count: ${count}`);
+		}
+
+		const r = possibleRatios[Math.floor(Math.random() * possibleRatios.length)];
+		const n = Math.random() < 0.5 ? 0 : 1;
+		const lives = r[n];
+		const blanks = r[1 - n];
+
+		for (let i = 0; i < lives; i++) {
+			this.bullets.push(new Bullet(1));
+		}
+		for (let i = 0; i < blanks; i++) {
+			this.bullets.push(new Bullet(0));
+		}
+
+		this.bullets.sort(() => Math.random() - 0.5);
+		// Shuffle
+	}
+}
+
+module.exports = Loader;
